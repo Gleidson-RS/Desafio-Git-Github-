e9d689be148025ac2f54946b6550a0e9f8a32a09
Gleidon <gleidsonrosasantos2@gmail.com>
2026-06-11 16:35:21 -0300

    concatenação, operadores matematicos, operadores racionais e logicos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,98 @@
 console.log("Teste")
+console.log("teste2")
+//ygiyuiiyryuiykgkiyutlig
+
+/*
+ie57i
+rgsrgsgr
+*/ 
+
+let idade = 17;
+const nome = "joao";
+
+console.log(nome, idade)
+
+//exemplo do professor
+
+let num = 50;
+var num2 = 130;
+const num3 = 450;
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+
+let num4 = 420
+var num5 = 180
+
+console.log(num4)
+console.log(num)
+}
+console.log(num5)
+
+num = 5
+
+console.log(num)
+
+num2 = 6
+
+console.log(num2)
+
+//concatenação
+
+console.log("valor de num", + num)
+console.log("Valor de num3", + num3)
+console.log(`Valor de num2 ${num2}`)
+console.log('A soma dos numeros', + 15 + 8) //correto
+console.log('A soma dos numeros', +(15 + 8)) //correto
+console.log('A soma dos numeros' + 15 + 8) //errado
+
+
+//operadores matematicos
+/*
+
++ soma
+-subtração
+*multiplicação
+/ divisão
+    % modulo(resto da divisão)
+*/
+console.log('Soma das Variaveis', num + num2)
+console.log('subtração das variaveis', num3 - num)
+console.log('multiplição', num5 * 2)
+console.log('divisão de valores', num2 / 5)
+console.log('resto da divisão', num2 % 2)
+console.log(`resto da divisão ${num3 % 3}`)
+
+/* operadores racionais
+
+  ==
+  > maior
+  < menor
+  >= maior igual
+  <=  menor igual
+  != direfente
+  
+*/
+
+// operadores logicos
+
+/*
+   && E
+   || OU
+ 
+*/
+
+console.log("TESTE DE RELACIONAIS", 10 == 5)
+console.log("TESTE DE RELACIONAIS", 10 != 5)
+console.log("TESTE DE RELACIONAIS", 10 > 5)
+console.log("TESTE DE RELACIONAIS", 10 < 5)
+console.log("TESTE DE RELACIONAIS", 10 >= 5)
+console.log("TESTE DE RELACIONAIS", 10 <= 5)
+console.log("TESTE LOGICO", (10 > 5) && (15 < 8))
+console.log("TESTE LOGICO", (10 > 5) || (15 < 8))
+
+
+
